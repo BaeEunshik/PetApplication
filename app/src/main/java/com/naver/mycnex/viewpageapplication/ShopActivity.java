4812c44d8b1b5a8fe941745043e7c8951d0d3bf4
kjhwert <kjhwert11@gmail.com>
2018-08-21 18:33:56 +0900

    2018.08.21 테스트 배은식

--- app/src/main/java/com/naver/mycnex/viewpageapplication/ShopActivity.java
+++ app/src/main/java/com/naver/mycnex/viewpageapplication/ShopActivity.java
@@ -3,11 +3,12 @@ package com.naver.mycnex.viewpageapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ShopInfo_Activity extends AppCompatActivity {
+public class ShopActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shop_info_);
+        setContentView(R.layout.activity_shop);
     }
+
 }
