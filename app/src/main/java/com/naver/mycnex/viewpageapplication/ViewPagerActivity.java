9baaab7370e6d16578fb6c03cf263cd84182abd0
kjhwert <kjhwert11@gmail.com>
2018-08-21 15:25:19 +0900

    2018.08.21 테스트 배은식

--- app/src/main/java/com/naver/mycnex/viewpageapplication/ViewPagerActivity.java
+++ app/src/main/java/com/naver/mycnex/viewpageapplication/ViewPagerActivity.java
@@ -31,10 +31,6 @@ import butterknife.Unbinder;
 public class ViewPagerActivity extends AppCompatActivity {
 
 
-    //&&&&&&&&
-    //????????
-    //?????
-
     ViewPagerAdapter viewPagerAdapter;
     Bus bus = BusProvider.getInstance().getBus();
     Unbinder unbinder;
