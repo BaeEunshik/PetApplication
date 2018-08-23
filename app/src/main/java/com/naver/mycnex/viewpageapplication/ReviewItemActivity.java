13b0851b5fdc01ce7263af1af6a80ea94b7596ba
mihwa <mycnex@gmail.com>
2018-08-23 19:48:15 +0900

    2018.08.23.pm 7:43 CMH -activity_review_item.xml -item_comment_list.xml -CommentListAdapter -ReviewItemActivity

--- app/src/main/java/com/naver/mycnex/viewpageapplication/ReviewItemActivity.java
+++ app/src/main/java/com/naver/mycnex/viewpageapplication/ReviewItemActivity.java
@@ -2,12 +2,50 @@ package com.naver.mycnex.viewpageapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.naver.mycnex.viewpageapplication.adapter.CommentListAdapter;
+import com.naver.mycnex.viewpageapplication.data.Comment;
+import java.util.ArrayList;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class ReviewItemActivity extends AppCompatActivity {
+    Unbinder unbinder;
+    ArrayList<Comment> items = new ArrayList<>();
+    CommentListAdapter commentListAdapter;
+
+    @BindView(R.id.btnGoBack)ImageButton btnGoBack;
+    @BindView(R.id.txt_nickname) TextView txt_nickname;
+    @BindView(R.id.txt_point) TextView txt_point;
+    @BindView(R.id.txt_content)TextView txt_content;
+    @BindView(R.id.txt_ComCount)TextView txt_ComCount;
+    @BindView(R.id.ibtn_review) ImageButton ibtn_review;
+    @BindView(R.id.ibtn_dialog1)ImageButton ibtn_dialog1;
+    @BindView(R.id.lv_comment_list)ListView lv_comment_list;
+    @BindView(R.id.btn_comment_save)Button btn_comment_save;
+    @BindView(R.id.et_input_comment)EditText et_input_comment;
+    @BindView(R.id.txt_comment)TextView txt_comment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_item);
+        unbinder = ButterKnife.bind(this);
+
+
+
+
+
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/naver/mycnex/viewpageapplication/adapter/CommentListAdapter.java
@@ -0,0 +1,59 @@
+package com.naver.mycnex.viewpageapplication.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+
+import com.naver.mycnex.viewpageapplication.R;
+import com.naver.mycnex.viewpageapplication.data.Comment;
+
+import java.util.ArrayList;
+
+public class CommentListAdapter extends BaseAdapter {
+    ArrayList<Comment> items;
+
+    public CommentListAdapter(ArrayList<Comment> items) {
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        Holder holder = new Holder();
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_comment_list, parent, false);
+            holder.txt_comment = convertView.findViewById(R.id.txt_comment);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (Holder)convertView.getTag();
+        }
+        Comment item = (Comment)getItem(position);
+
+        holder.txt_comment.setText(item.getComment());
+
+        return convertView;
+    }
+
+    public class Holder {
+        TextView txt_comment;
+    }
+}
