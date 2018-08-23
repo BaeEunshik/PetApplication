1cf4c4e5c5416321e57076718fcecd49d762f780
kjhwert <kjhwert11@naver.com>
2018-08-23 17:08:44 +0900

    mainfest 수정

--- app/src/main/java/com/naver/mycnex/viewpageapplication/RegisterShopActivity.java
+++ app/src/main/java/com/naver/mycnex/viewpageapplication/RegisterShopActivity.java
@@ -1,29 +1,51 @@
 package com.naver.mycnex.viewpageapplication;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.content.ClipData;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 import com.naver.mycnex.viewpageapplication.Address.Address;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import butterknife.BindDimen;
 import butterknife.BindView;
@@ -34,7 +56,9 @@ import lombok.Builder;
 
 public class RegisterShopActivity extends AppCompatActivity {
 
-    public static Integer ADDRESS_SUBMIT_OK = 2;
+    private final int ADDRESS_SUBMIT = 11;
+    private final int CAMERA_GALLERY = 111;
+
     public static Integer RESERVATION_ABLE = 0;  // 예약가능 : 0 , 예약불가 : 1
     public static Integer RESERVATION_UNABLE = 1;
     public static Integer RESERVATION = RESERVATION_ABLE;
@@ -42,20 +66,20 @@ public class RegisterShopActivity extends AppCompatActivity {
     public static Integer PETSIZE_SMALL = 1;
     public static Integer PETSIZE_MIDDLE = 2;
     public static Integer PETSIZE_LARGE = 3;
-    public static Integer PARKING = 0;
     public static Integer PARKING_ABLE = 1;
     public static Integer PARKING_UNABLE = 2;
     public static Integer PARKING_VALET = 3;
+    public static Integer PARKING = PARKING_UNABLE;
     public static String OPERATION_DATE = "";
-    public static Integer MONDAY = 0;
-    public static Integer TUESDAY = 1;
-    public static Integer WEDNESDAY = 2;
-    public static Integer THURSDAY = 3;
-    public static Integer FRIDAY = 4;
-    public static Integer SATURDAY = 5;
-    public static Integer SUNDAY = 6;
+    public static String PHONE_NUMBER = "";
+
+    double lat = 0;
+    double lng = 0;
+
     boolean[] bool_date;
     String[] str_date = {"월","화","수","목","금","토","일"};
+    ImageView[] images = new ImageView[6];
+    Button[] buttons = new Button[7];
 
     Unbinder unbinder;
     @BindView(R.id.middlePhone_edit) EditText middlePhone_edit;
@@ -82,6 +106,14 @@ public class RegisterShopActivity extends AppCompatActivity {
     @BindView(R.id.btnSaturday) Button btnSaturday;
     @BindView(R.id.btnSunday) Button btnSunday;
     @BindView(R.id.submit_btn) Button submit_btn;
+    @BindView(R.id.select_photo_btn) Button select_photo_btn;
+
+    @BindView(R.id.image1) ImageView image1;
+    @BindView(R.id.image2) ImageView image2;
+    @BindView(R.id.image3) ImageView image3;
+    @BindView(R.id.image4) ImageView image4;
+    @BindView(R.id.image5) ImageView image5;
+    @BindView(R.id.image6) ImageView image6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,8 +122,6 @@ public class RegisterShopActivity extends AppCompatActivity {
         //버터나이프
         unbinder = ButterKnife.bind(this);
         InitWhenCreated();
-
-
 
     }
 
@@ -103,15 +133,34 @@ public class RegisterShopActivity extends AppCompatActivity {
         unbinder.unbind();
     }
 
-    @Override // 주소 정보 받아오기
+    @Override // Activity Result
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ADDRESS_SUBMIT_OK) {
+        if (requestCode == ADDRESS_SUBMIT) {
             if (resultCode == RESULT_OK) {
                 String address = data.getStringExtra("address");
                 btn_shop_address.setText(address);
             }
+        } else if (requestCode == CAMERA_GALLERY) {
+            if (resultCode == RESULT_OK) {
+
+                ClipData clipData = data.getClipData();
+
+                if (clipData != null) {
+
+                    InitImage();
+                    select_photo_btn.setVisibility(View.GONE);
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        Uri urione =  clipData.getItemAt(i).getUri();
+                        images[i].setImageURI(urione);
+                        images[i].setVisibility(View.VISIBLE);
+                    }
+
+                }
+            }
         }
+
+
     }
 
     /********** OnClick **********/
@@ -123,7 +172,7 @@ public class RegisterShopActivity extends AppCompatActivity {
     public void shop_address(){
         //다음주소검색 API activity 로 이동
         Intent intent = new Intent(RegisterShopActivity.this, Address.class);
-        startActivityForResult(intent,ADDRESS_SUBMIT_OK);
+        startActivityForResult(intent,ADDRESS_SUBMIT);
     }
 
     @OnClick(R.id.btnReserveOk)    // 개인 / 업체 가입자 버튼 클릭 이벤트
@@ -237,114 +286,18 @@ public class RegisterShopActivity extends AppCompatActivity {
         timePickerDialog.show();
     }
 
-    // 운영요일
-    @OnClick(R.id.btnMonday)
-    public void btnMonday() {
-
-        if (bool_date[MONDAY]) {
-
-            btnMonday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_green));
-            btnMonday.setTextColor(Color.parseColor("#FFFFFF"));
-
-            bool_date[MONDAY] = false;
-        } else {
-
-            btnMonday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_white));
-            btnMonday.setTextColor(Color.parseColor("#000000"));
-
-            bool_date[MONDAY] = true;
-        }
-    }
-    @OnClick(R.id.btnTuesday)
-    public void btnTuesday() {
-        if (bool_date[TUESDAY]) {
-            btnTuesday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_green));
-            btnTuesday.setTextColor(Color.parseColor("#FFFFFF"));
-
-            bool_date[TUESDAY] = false;
-        } else {
-            btnTuesday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_white));
-            btnTuesday.setTextColor(Color.parseColor("#000000"));
-
-            bool_date[TUESDAY] = true;
-        }
-    }
-    @OnClick(R.id.btnThursday)
-    public void btnThursday() {
-        if (bool_date[THURSDAY]) {
-            btnThursday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_green));
-            btnThursday.setTextColor(Color.parseColor("#FFFFFF"));
-
-            bool_date[THURSDAY] = false;
-        } else {
-            btnThursday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_white));
-            btnThursday.setTextColor(Color.parseColor("#000000"));
-
-            bool_date[THURSDAY] = true;
-        }
-    }
-    @OnClick(R.id.btnWednesday)
-    public void btnWednesday() {
-        if (bool_date[WEDNESDAY]) {
-            btnWednesday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_green));
-            btnWednesday.setTextColor(Color.parseColor("#FFFFFF"));
-
-            bool_date[WEDNESDAY] = false;
-        } else {
-            btnWednesday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_white));
-            btnWednesday.setTextColor(Color.parseColor("#000000"));
-
-            bool_date[WEDNESDAY] = true;
-        }
-    }
-    @OnClick(R.id.btnFriday)
-    public void btnFriday() {
-        if (bool_date[FRIDAY]) {
-            btnFriday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_green));
-            btnFriday.setTextColor(Color.parseColor("#FFFFFF"));
-
-            bool_date[FRIDAY] = false;
-        } else {
-            btnFriday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_white));
-            btnFriday.setTextColor(Color.parseColor("#000000"));
-
-            bool_date[FRIDAY] = true;
-        }
-    }
-    @OnClick(R.id.btnSunday)
-    public void btnSunday() {
-        if (bool_date[SUNDAY]) {
-            btnSunday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_green));
-            btnSunday.setTextColor(Color.parseColor("#FFFFFF"));
-
-            bool_date[SUNDAY] = false;
-        } else {
-            btnSunday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_white));
-            btnSunday.setTextColor(Color.parseColor("#000000"));
-
-            bool_date[SUNDAY] = true;
-        }
-    }
-    @OnClick(R.id.btnSaturday)
-    public void btnSaturday() {
-        if (bool_date[SATURDAY]) {
-            btnSaturday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_green));
-            btnSaturday.setTextColor(Color.parseColor("#FFFFFF"));
-
-            bool_date[SATURDAY] = false;
-        } else {
-            btnSaturday.setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_white));
-            btnSaturday.setTextColor(Color.parseColor("#000000"));
-
-            bool_date[SATURDAY] = true;
-        }
+    @OnClick(R.id.select_photo_btn)
+    public void select_photo_btn() {
+        Permission();
     }
 
-    @OnClick(R.id.submit_btn)   //등록
+    /*************** 등록 버튼 ******************/
+    @OnClick(R.id.submit_btn)
     public void submit() {
 
         String name = shop_name.getText().toString();
         String address = btn_shop_address.getText().toString();
+        getLatLng();
 
         for (int i = 0; i < bool_date.length; i++) {
             if (bool_date[i] == false) {
@@ -354,7 +307,35 @@ public class RegisterShopActivity extends AppCompatActivity {
 
         String startTime = startTime_btn.getText().toString();
         String endTime = endTime_btn.getText().toString();
-        String phone = middlePhone_edit.getText().toString() + lastPhone_edit.getText().toString();
+        PHONE_NUMBER += "-" + middlePhone_edit.getText().toString() + "-" + lastPhone_edit.getText().toString();
+
+        if (name == null || name.equals("")) {
+            Toast.makeText(RegisterShopActivity.this, "가게명을 입력하세요", Toast.LENGTH_SHORT).show();
+            shop_name.requestFocus();
+            shop_name.setFocusableInTouchMode(true);
+            return;
+        }
+
+        if (address == null || address.equals("")) {
+            Toast.makeText(RegisterShopActivity.this, "주소를 입력하세요", Toast.LENGTH_SHORT).show();
+            btn_shop_address.requestFocus();
+            btn_shop_address.setFocusableInTouchMode(true);
+            return;
+        }
+
+        if (OPERATION_DATE == "") {
+            Toast.makeText(RegisterShopActivity.this, "영업시간을 선택하세요", Toast.LENGTH_SHORT).show();
+            btnMonday.requestFocus();
+            btnMonday.setFocusableInTouchMode(true);
+            return;
+        }
+
+        if (PHONE_NUMBER.length() < 9) {
+            Toast.makeText(RegisterShopActivity.this, "번호를 입력하세요", Toast.LENGTH_SHORT).show();
+            middlePhone_edit.requestFocus();
+            middlePhone_edit.setFocusableInTouchMode(true);
+            return;
+        }
 
         Log.d("asd",name);
         Log.d("asd", address);
@@ -372,7 +353,7 @@ public class RegisterShopActivity extends AppCompatActivity {
         } else {
             Log.d("asd","RESERVATION_UNABLE");
         }
-        Log.d("asd",phone);
+        Log.d("asd",PHONE_NUMBER);
 
         if (PARKING == PARKING_ABLE) {
             Log.d("asd","PARKING_ABLE");
@@ -382,14 +363,10 @@ public class RegisterShopActivity extends AppCompatActivity {
             Log.d("asd","PARKING_VALET");
         }
 
-        // Intent 사용하여 새 액티비티 띄우면 : ViewPagerActivity 기존것과 함께 2장 띄워진 상황 ㅜㅜ
-        /*Intent intent = new Intent(RegisterShopActivity.this, ViewPagerActivity.class);
-        startActivity(intent);*/
-        // 변경)
-        Intent intent = new Intent(RegisterShopActivity.this, ShopActivity.class);
-        finish();
-        startActivity(intent);
+        Log.d("asd", String.valueOf(lat) + " " + String.valueOf(lng));
 
+//        Intent intent = new Intent(RegisterShopActivity.this, ShopActivity.class);
+//        startActivity(intent);
     }
     /********** METHOD **********/
 
@@ -439,6 +416,7 @@ public class RegisterShopActivity extends AppCompatActivity {
         }
     }
 
+    // Oncreated시 초기화(initiation)
     public void InitWhenCreated() {
         // 핸드폰 번호 카테고리
         ArrayAdapter addressAdapter = ArrayAdapter.createFromResource(this, R.array.phoneNumSelect, android.R.layout.simple_spinner_item);
@@ -450,6 +428,77 @@ public class RegisterShopActivity extends AppCompatActivity {
         for (int i = 0; i < bool_date.length; i++) {
             bool_date[i] = true;
         }
+
+        DateOnclickListener();
+
+        spinnerPhone.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                PHONE_NUMBER = String.valueOf(parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        getLastPhoneNumberFocus();
+        Parking();
+
+        images[0] = image1;
+        images[1] = image2;
+        images[2] = image3;
+        images[3] = image4;
+        images[4] = image5;
+        images[5] = image6;
+
+        InitImage();
+
+        for (int i = 0; i < images.length; i++) {
+            images[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    IntentToCameraGAllery();
+                }
+            });
+        }
+
+    }
+
+    public void getLastPhoneNumberFocus() {
+        middlePhone_edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (middlePhone_edit.length() == 4) {
+                    lastPhone_edit.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public void InitImage() {
+        for (int i = 0; i < 6; i++) {
+            images[i].setImageResource(0);
+            images[i].setVisibility(View.GONE);
+        }
+    }
+
+    public void IntentToCameraGAllery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),  CAMERA_GALLERY);
     }
 
     public void Parking() {
@@ -490,4 +539,71 @@ public class RegisterShopActivity extends AppCompatActivity {
         }
     }
 
+    public void Permission() {
+        PermissionListener permissionlistener = new PermissionListener() {
+            //권한 획득시
+            @Override
+            public void onPermissionGranted() {
+
+                IntentToCameraGAllery();
+
+            }
+            //권한 거부시
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                Toast.makeText(RegisterShopActivity.this, "권한 거부\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("갤러리 접근 권한을 거부하셨습니다. \n[설정] > [권한]에서 권한을 허용할 수 있습니다.")
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .check();
+    }
+
+    public void getLatLng() {
+        Geocoder geocoder = new Geocoder(RegisterShopActivity.this);
+
+        try {
+
+            List<android.location.Address> addr = geocoder.getFromLocationName( btn_shop_address.getText().toString(),1); // 주소가 저장된 값을 넣으면 된다. Address 는 안드로이드가 제공하는 객체.
+            lat = addr.get(0).getLatitude();
+            lng = addr.get(0).getLongitude();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void DateOnclickListener() {
+        buttons[0] = btnMonday;
+        buttons[1] = btnTuesday;
+        buttons[2] = btnWednesday;
+        buttons[3] = btnThursday;
+        buttons[4] = btnFriday;
+        buttons[5] = btnSaturday;
+        buttons[6] = btnSunday;
+
+        for (int i = 0; i < buttons.length; i++) {
+            final int finalI = i;
+            buttons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (bool_date[finalI]) {
+
+                        buttons[finalI].setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_green));
+                        buttons[finalI].setTextColor(Color.parseColor("#FFFFFF"));
+
+                        bool_date[finalI] = false;
+                    } else {
+
+                        buttons[finalI].setBackground(ContextCompat.getDrawable(RegisterShopActivity.this,R.drawable.button_left_white));
+                        buttons[finalI].setTextColor(Color.parseColor("#000000"));
+
+                        bool_date[finalI] = true;
+                    }
+                }
+            });
+        }
+    }
 }
