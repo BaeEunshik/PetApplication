bb6b6eaba50a086ab9619bb37de33995db1d7586
kjhwert <global>
2018-08-28 18:16:32 +0900

    2018 8 28 Store 객체 서버 연동.
    ViewPagerActivity, RegisterShopActivity, VPFragment1

--- app/src/main/java/com/naver/mycnex/viewpageapplication/retrofit/RetrofitRequest.java
+++ app/src/main/java/com/naver/mycnex/viewpageapplication/retrofit/RetrofitRequest.java
@@ -1,8 +1,13 @@
 package com.naver.mycnex.viewpageapplication.retrofit;
 
+import com.naver.mycnex.viewpageapplication.data.Store;
+
+import java.util.ArrayList;
+
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 /**
@@ -18,6 +23,19 @@ public interface RetrofitRequest {
     @FormUrlEncoded
     @POST("login.do")
     Call<Boolean> login(@Field("login_id") String login_id, @Field("login_pw") String login_pw);
+
+    @FormUrlEncoded
+    @POST("submitStore.do")
+    Call<Long> submitStore(@Field("name")String name, @Field("contact") String contact, @Field("dog_size") Integer dog_size, @Field("store_information")String info,
+                           @Field("operation_day")String operation_day, @Field("operation_time") String operation_time, @Field("parking") Integer parking, @Field("reservation") Integer reservation,
+                           @Field("address")String address, @Field("sigungu")String sigungu, @Field("dong") String dong, @Field("lat") double lat, @Field("lng") double lng, @Field("category") Integer category);
+
+    @FormUrlEncoded
+    @POST("storeDetail.do")
+    Call<Store> storeDetail(@Field("id")long id);
+
+    @GET("getStoreData.do")
+    Call<ArrayList<Store>> getStoreData();
 
     /* 파일 업로드
     @Multipart
